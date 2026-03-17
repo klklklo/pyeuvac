26e7fb9425ca92c9274d10ebd6101331798e4388
klklklo <68776358+klklklo@users.noreply.github.com>
2026-03-17 20:12:36 +0300

    Update

--- src/pyeuvac/_euvac.py
+++ src/pyeuvac/_euvac.py
@@ -1,5 +1,6 @@
 import numpy as np
 import xarray as xr
+import sparse
 import pyeuvac._misc as _m
 
 
@@ -21,16 +22,6 @@ class Euvac:
         self._lines_ai = np.array(self._lines_dataset['Ai'], dtype=np.float64).reshape(17,1)
 
     def _get_p(self, i, f107, f107avg):
-        '''
-        Method for getting the P parameter. For each pair of values f107 and f107a, the value
-        P = (f107 + f107 avg) / 2. - 80. is calculated, and then a matrix is constructed,
-        each column of which is the value P.
-        :param i: number of rows.
-        :param f107: single value of the daily index F10.7 (in s.f.u.) or an array of such values.
-        :param f107avg: a single value of the index F10.7 (in s.f.u.) averaged over 81 days or an array of such values.
-        :return: numpy array.
-        '''
-
         if f107.size != f107avg.size:
             raise Exception(f'The number of F10.7 and F10.7_avg values does not match. f107 contained {f107.size} '
                             f'elements, f107avg contained {f107avg.size} elements.')
@@ -58,30 +49,32 @@ class Euvac:
 
     def get_spectral_bands(self, *, f107, f107avg, correction=False):
         bands = 20
-        if self._check_types(f107, f107avg):
+        cols = len(f107)
 
+        if self._check_types(f107, f107avg):
             f107 = np.array([f107], dtype=np.float64) if isinstance(f107, (type(None), int, float)) \
                 else np.array(f107, dtype=np.float64)
             f107avg = np.array([f107avg], dtype=np.float64) if isinstance(f107avg, (type(None), int, float)) \
                 else np.array(f107avg, dtype=np.float64)
 
             p = self._get_p(bands,f107, f107avg)
-            spectra = self._predict(self._bands_ai, self._bands_f74113, p, correction)
+            spectra = self._predict(self._bands_ai, self._bands_f74113, p, correction).flatten(order='F')
 
-            res = np.zeros((spectra.shape[1], spectra.shape[1], spectra.shape[0]))
-            for i in range(spectra.shape[1]):
-                res[i, i, :] = spectra[:, i]
+            coords = np.array([np.hstack([x for x in [np.repeat(i, bands) for i in range(cols)]]),
+                               np.hstack([x for x in [np.repeat(i, bands) for i in range(cols)]]),
+                               np.hstack([x for x in [np.arange(bands) for _ in range(cols)]])])
 
-            return xr.Dataset(data_vars={'euv_flux_spectra': (('F107', 'F107AVG', 'band_center'), res),
-                                         'lband': ('band_number', self._bands_dataset['lband'].values),
-                                         'uband': ('band_number', self._bands_dataset['uband'].values)},
+            s = sparse.COO(coords, spectra, shape=(cols, cols, bands))
+
+
+            return xr.Dataset(data_vars={'euv_flux_spectra': (('F107', 'F107AVG', 'band_center'), s)},
                               coords={'F107': f107,
                                       'F107AVG':  f107avg,
                                       'band_center': self._bands_dataset['center'].values,
                                       'band_number': np.arange(bands)},
                               attrs={'F10.7 units': '10^-22 · W · m^-2 · Hz^-1',
                                      'F10.7 81-day average units': '10^-22 · W · m^-2 · Hz^-1',
-                                     'spectra units': 'photons · m^-2 · s^-1',
+                                     'spectrum units': 'photons · m^-2 · s^-1',
                                      'wavelength units': 'nm',
                                      'euv_flux_spectra': 'modeled EUV solar irradiance',
                                      'lband': 'lower boundary of wavelength interval',
@@ -89,6 +82,8 @@ class Euvac:
 
     def get_spectral_lines(self, *, f107, f107avg, correction=False):
         lines = 17
+        cols = len(f107)
+
         if self._check_types(f107, f107avg):
             f107 = np.array([f107], dtype=np.float64) if isinstance(f107, (type(None), int, float)) \
                 else np.array(f107, dtype=np.float64)
@@ -96,13 +91,15 @@ class Euvac:
                 else np.array(f107avg, dtype=np.float64)
 
             p = self._get_p(lines, f107, f107avg)
-            spectra = self._predict(self._lines_ai, self._lines_f74113, p, correction)
+            spectra = self._predict(self._lines_ai, self._lines_f74113, p, correction).flatten(order='F')
 
-            res = np.zeros((spectra.shape[1], spectra.shape[1], spectra.shape[0]))
-            for i in range(spectra.shape[1]):
-                res[i, i, :] = spectra[:, i]
+            coords = np.array([np.hstack([x for x in [np.repeat(i, lines) for i in range(cols)]]),
+                               np.hstack([x for x in [np.repeat(i, lines) for i in range(cols)]]),
+                               np.hstack([x for x in [np.arange(lines) for _ in range(cols)]])])
 
-            return xr.Dataset(data_vars={'euv_flux_spectra': (('F107', 'F107AVG', 'line_wavelength'), res),
+            s = sparse.COO(coords, spectra, shape=(cols, cols, lines))
+
+            return xr.Dataset(data_vars={'euv_flux_spectra': (('F107', 'F107AVG', 'line_wavelength'), s),
                                          'wavelength': ('line_number', self._lines_dataset['lambda'].values)},
                               coords={'F107': f107,
                                       'F107AVG': f107avg,
@@ -110,7 +107,7 @@ class Euvac:
                                       'line_number': np.arange(lines)},
                               attrs={'F10.7 units': '10^-22 · W · m^-2 · Hz^-1',
                                      'F10.7 81-day average units': '10^-22 · W · m^-2 · Hz^-1',
-                                     'spectra units': 'photons · m^-2 · s^-1',
+                                     'spectrum units': 'photons · m^-2 · s^-1',
                                      'wavelength units': 'nm',
                                      'euv_flux_spectra': 'modeled EUV solar irradiance',
                                      'wavelength': 'the wavelength of a discrete line'})
@@ -119,23 +116,26 @@ class Euvac:
         return (self.get_spectral_bands(f107=f107, f107avg=f107avg, correction=correction),
                 self.get_spectral_lines(f107=f107, f107avg=f107avg, correction=correction))
 
-    def predict(self, f107, f107avg, correction=False):
+    def predict(self, *, f107, f107avg, correction=False):
         data = 37
-        if self._check_types(f107, f107avg):
+        cols = len(f107)
 
+        if self._check_types(f107, f107avg):
             f107 = np.array([f107], dtype=np.float64) if isinstance(f107, (type(None), int, float)) \
                 else np.array(f107, dtype=np.float64)
             f107avg = np.array([f107avg], dtype=np.float64) if isinstance(f107avg, (type(None), int, float)) \
                 else np.array(f107avg, dtype=np.float64)
 
             p = self._get_p(data, f107, f107avg)
-            spectra = self._predict(self._full_ai, self._full_f74113, p, correction)
+            spectra = self._predict(self._full_ai, self._full_f74113, p, correction).flatten(order='F')
 
-            res = np.zeros((spectra.shape[1], spectra.shape[1], spectra.shape[0]))
-            for i in range(spectra.shape[1]):
-                res[i, i, :] = spectra[:, i]
+            coords = np.array([np.hstack([x for x in [np.repeat(i, data) for i in range(cols)]]),
+                               np.hstack([x for x in [np.repeat(i, data) for i in range(cols)]]),
+                               np.hstack([x for x in [np.arange(data) for _ in range(cols)]])])
 
-            return xr.Dataset(data_vars={'euv_flux_spectra': (('F107', 'F107AVG', 'band_center'), res),
+            s = sparse.COO(coords, spectra, shape=(cols, cols, data))
+
+            return xr.Dataset(data_vars={'euv_flux_spectra': (('F107', 'F107AVG', 'band_center'), s),
                                          'lband': ('band_number', self._full_dataset['lband'].values),
                                          'uband': ('band_number', self._full_dataset['uband'].values)},
                               coords={'F107': f107,
@@ -144,7 +144,7 @@ class Euvac:
                                       'band_number': np.arange(data)},
                               attrs={'F10.7 units': '10^-22 · W · m^-2 · Hz^-1',
                                      'F10.7 81-day average units': '10^-22 · W · m^-2 · Hz^-1',
-                                     'spectra units': 'photons · m^-2 · s^-1',
+                                     'spectrum units': 'photons · m^-2 · s^-1',
                                      'wavelength units': 'nm',
                                      'euv_flux_spectra': 'modeled EUV solar irradiance',
                                      'lband': 'lower boundary of wavelength interval',
